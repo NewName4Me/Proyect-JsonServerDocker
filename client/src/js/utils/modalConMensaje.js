c684c0ef8a8a3d2efaa4eaf460adbb30653d3b0b
NewName4ME <tortarod@gmail.com>
2024-12-19 17:05:57 +0100

    ADD: funciones encargadas de mostrar diferentes tipos de alerta

---
+++ client/src/js/utils/modalConMensaje.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @param {ListDeMensajesDispoiblesEnum} msg 
+ * @param {ListaDeTiposDeAlertaEnum} type 
+ */
+function modalConMensaje(msg, type) {
+    const alerta = document.createElement('DIV');
+    alerta.classList.add(`alert-${type}`);
+    alerta.textContent = msg;
+    document.body.appendChild(alerta);
+    setTimeout(() => alerta.remove(), 3000);
+}
+
+const ListDeMensajesDispoiblesEnum = Object.freeze({
+    ITEM_AGREGADO_AL_CARRITO: 'Agregado al carrito de forma exitosa',
+    ITEM_ELIMINADO_DEL_CARRITO: 'Eliminado del carrito de forma exitosa',
+});
+
+const ListaDeTiposDeAlertaEnum = Object.freeze({
+    SUCCESS: 'SUCCESS',
+    INFO: 'INFO',
+    WARNING: 'WARNING',
+    ERROR: 'ERROR'
+});
