3813be6ec601524bc58dc5d7dff2c5ded8462c0d
NewName4ME <tortarod@gmail.com>
2024-12-27 18:42:57 +0100

    UPDATE: agregar animacion al login para que este parezca que se mueva al cambiar de seccion

--- client/src/views/login/app.js
+++ client/src/views/login/app.js
@@ -1,11 +1,35 @@
 import { handleLoginFormSubmitController } from '../../js/controllers/loginController.js';
-import { ListDeMensajesDispoiblesEnum, ListaDeTiposDeAlertaEnum, modalConMensaje } from '../../js/utils/modalConMensaje.js';
+import {
+    ListDeMensajesDispoiblesEnum,
+    ListaDeTiposDeAlertaEnum,
+    modalConMensaje
+} from '../../js/utils/modalConMensaje.js';
 import { handleCaptchaSubmit } from '../../js/controllers/captchaController.js';
 
 document.addEventListener('DOMContentLoaded', startApp);
 
 function startApp() {
     const $loginForm = document.forms.loginForm;
+    const $nextBtn = $loginForm.querySelector('.next-btn');
+    const $backBtn = $loginForm.querySelector('.back-btn');
+    const $step1 = $loginForm.querySelector('.step-1');
+    const $step2 = $loginForm.querySelector('.step-2');
+
+    $nextBtn.addEventListener('click', () => {
+        const email = $loginForm.useremail.value;
+        if (email) {
+            $step1.classList.add('previous');
+            $step1.classList.remove('active');
+            $step2.classList.add('active');
+        }
+    });
+
+    $backBtn.addEventListener('click', () => {
+        $step2.classList.remove('active');
+        $step1.classList.remove('previous');
+        $step1.classList.add('active');
+    });
+
     $loginForm.addEventListener('submit', async (e) => {
         const correct = await handleLoginFormSubmitController(e, $loginForm);
 
@@ -23,7 +47,7 @@ function mostrarReCaptcha() {
     captchaContainer.classList.add('captcha');
 
     const captchaParagrap = document.createElement('P');
-    captchaParagrap.textContent = Math.random().toString(32).substring(0,6);
+    captchaParagrap.textContent = Math.random().toString(32).substring(0, 6);
 
     const userInput = document.createElement('INPUT');
     userInput.type = "text";
@@ -39,4 +63,3 @@ function mostrarReCaptcha() {
 
     document.body.appendChild(captchaContainer);
 }
-
