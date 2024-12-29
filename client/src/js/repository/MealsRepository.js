7694a4af66088275d89079e82bb32e98039d1d02
NewName4ME <tortarod@gmail.com>
2024-12-29 16:15:02 +0100

    FIX: error para ordenar comida por categorias

--- client/src/js/repository/MealsRepository.js
+++ client/src/js/repository/MealsRepository.js
@@ -3,27 +3,23 @@ import { API_MEALS_URI } from '../../../config/env.js';
 export class MealsRepository {
 
     #Meals_API = API_MEALS_URI;
+    #OrderMap;
+
+    constructor() {
+        this.#OrderMap = Object.freeze({
+            'sin': '',
+            'asc': 'price',
+            'desc': '-price'
+        });
+    }
 
     async getMeals(page = 1, items_per_page = 8, order = null) {
-        let data;
-
-        switch (order) {
-            case 'sin':
-                data = await fetch(`${this.#Meals_API}?_page=${page}&_per_page=${items_per_page}`);
-                break;
-            case 'asc':
-                data = await fetch(`${this.#Meals_API}?_sort=price&_page=${page}&_per_page=${items_per_page}`);
-                break;
-            case 'desc':
-                data = await fetch(`${this.#Meals_API}?_sort=-price&_page=${page}&_per_page=${items_per_page}`);
-                break;
-            default: throw new Error('Forma de ordenar no valida');
-        }
+        const data = await fetch(`${this.#Meals_API}?_sort=${this.#OrderMap[order]}&_page=${page}&_per_page=${items_per_page}`);
         return await data.json() || null;
     }
 
     async getMealsFiltedByCategory(category = '', page = 1, items_per_page = 8, order = null) {
-        const data = await fetch(`${this.#Meals_API}?_page=${page}&_per_page=${items_per_page}&strCategory=${category}`);
+        const data = await fetch(`${this.#Meals_API}?_sort=${this.#OrderMap[order]}&_page=${page}&_per_page=${items_per_page}&strCategory=${category}`);
         return await data.json();
     }
 
