e0e78441e147acbc22670c14d47083b441bea7a2
NewName4ME <tortarod@gmail.com>
2025-01-04 19:03:07 +0100

    UPDATE: convertir cabecera en menu hamburguesa para pantallas pequeñas

--- client/config/env.js
+++ client/config/env.js
@@ -1,3 +1,3 @@
-export const API_USERS_URI = 'http://35.173.190.237:3000/users';
-export const API_CATEGORIES_URI = 'http://35.173.190.237:3002/categories';
-export const API_MEALS_URI = 'http://35.173.190.237:3001/meals'; 
+export const API_USERS_URI = 'http://localhost:3000/users';
+export const API_CATEGORIES_URI = 'http://localhost:3002/categories';
+export const API_MEALS_URI = 'http://localhost:3001/meals'; 
