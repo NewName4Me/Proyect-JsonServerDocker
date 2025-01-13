02666047f07464904d5fe6c9aec87206c077cd39
NewName4ME <tortarod@gmail.com>
2025-01-13 14:52:36 +0100

    FIX: rutas imagenes footer en las views

--- client/config/env.js
+++ client/config/env.js
@@ -1,3 +1,3 @@
-export const API_USERS_URI = 'http://52.90.244.163:3000/users';
-export const API_CATEGORIES_URI = 'http://52.90.244.163:3002/categories';
-export const API_MEALS_URI = 'http://52.90.244.163:3001/meals'; 
+export const API_USERS_URI = 'http://localhost:3000/users';
+export const API_CATEGORIES_URI = 'http://localhost:3002/categories';
+export const API_MEALS_URI = 'http://localhost:3001/meals'; 
