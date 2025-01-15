9c25d578e1e6c300599c87f20534fb7faf8f5883
NewName4ME <tortarod@gmail.com>
2025-01-15 01:56:41 +0100

    TEST: agregar un proxy para poder usar los archivos de mi imagen de docker con https

--- deploy/command.sh
+++ deploy/command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# instalaciones
+# Instalaciones
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y nodejs npm git apache2 apache2-utils openssl
@@ -9,10 +9,10 @@ sudo npm i -g n
 sudo n latest
 sudo systemctl restart apache2
 
-# tomar todo del repositorio
+# Tomar todo del repositorio
 sudo git clone https://github.com/NewName4Me/Proyect-JsonServerDocker.git /tmp/mi_repositorio
 
-# instalar docker
+# Instalar docker
 sudo apt-get update -y
 sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -25,11 +25,11 @@ echo \
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-#configuracion servidor apache con pagina web
+# Configuración servidor Apache con página web
 sudo rm -rf /var/www/html/* 
 sudo cp -r /tmp/mi_repositorio/client/ /var/www/html/
 
-#agregar configuracion del servidor para que use el dominio que necesitamos
+# Agregar configuración del servidor para que use el dominio que necesitamos
 echo "<VirtualHost *:80>
     ServerAdmin mail@codewithsusan.com
     DocumentRoot /var/www/
@@ -51,7 +51,7 @@ cd /etc/apache2/sites-available/
 sudo a2ensite tortarod.shop.conf
 sudo systemctl reload apache2
 
-#configurar para ssl
+# Configurar para SSL
 sudo mkdir -p /etc/apache2/ssl/tortarod.shop
 sudo cp /tmp/certificate.crt /etc/apache2/ssl/tortarod.shop/certificate.crt
 sudo cp /tmp/private.key /etc/apache2/ssl/tortarod.shop/private.key
@@ -87,13 +87,21 @@ echo "<VirtualHost *:80>
     SSLSessionTickets off
 
     Header always set Strict-Transport-Security "max-age=63072000"
+
+    # Configuración del proxy inverso para Docker
+    ProxyPass "/api" "http://localhost:3000/"
+    ProxyPassReverse "/api" "http://localhost:3000/"
+    ProxyPass "/meals" "http://localhost:3001/"
+    ProxyPassReverse "/meals" "http://localhost:3001/"
+    ProxyPass "/categories" "http://localhost:3002/"
+    ProxyPassReverse "/categories" "http://localhost:3002/"
 </VirtualHost>" | sudo tee /etc/apache2/sites-available/tortarod.shop.conf
 sudo a2ensite tortarod.shop.conf
 sudo a2dissite 000-default.conf
 sudo apache2ctl configtest
 sudo systemctl restart apache2
 
-# ejecutar Dockerfile en carpeta db(esto debe ejecutarse al final siempre)
+# Ejecutar Dockerfile en carpeta db (esto debe ejecutarse al final siempre)
 cd /tmp/mi_repositorio/db/
-sudo docker build -t json-server-multi .
+sudo docker build -t json-server-multi . 
 sudo docker run --rm -p 3000:3000 -p 3001:3001 -p 3002:3002 json-server-multi
