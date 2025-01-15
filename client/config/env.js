df3419cd0564277b74488f2d398cd3704d9b1e86
NewName4ME <tortarod@gmail.com>
2025-01-15 03:15:39 +0100

    UPDATE: actualizar endpoints

--- client/config/env.js
+++ client/config/env.js
@@ -1,3 +1,3 @@
-export const API_USERS_URI = 'http://54.89.238.147:3000/users';
-export const API_CATEGORIES_URI = 'http://54.89.238.147:3002/categories';
-export const API_MEALS_URI = 'http://54.89.238.147:3001/meals'; 
+export const API_USERS_URI = 'https://tortarod.shop/api';
+export const API_CATEGORIES_URI = 'https://tortarod.shop/categories';
+export const API_MEALS_URI = 'https://tortarod.shop/meals';
